55181d26e15b00a1c387d81c5599a763b4554a17
luoxp <luoxp@jugame.cn>
2015-06-19 16:48:37 +0800

    first

---
+++ src/com/example/webviewdemo/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.webviewdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private WebView mWebView;
+	private WebSettings mWebSettings;
+	private String urlStr = "http://www.baidu.com/";
+	private ProgressBar mProgressBar;
+	private TextView titleTextView;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setMax(100);
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				return false;
+			}
+        	
+        });
+        mWebView.setWebChromeClient(new WebChromeClient(){
+
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				if (newProgress == 100) {
+					mProgressBar.setVisibility(View.INVISIBLE);
+				} else {
+					mProgressBar.setVisibility(View.VISIBLE);
+					mProgressBar.setProgress(newProgress);
+				}
+			}
+
+			@Override
+			public void onReceivedTitle(WebView view, String title) {
+				titleTextView.setText(title);
+			}
+        	
+        });
+        mWebView.loadUrl(urlStr);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
