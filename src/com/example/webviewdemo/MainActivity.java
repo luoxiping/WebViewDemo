f67a3df92dd56ba3be6f5496f6898e7a0d3ca6c7
luoxp <luoxp@jugame.cn>
2015-06-19 16:59:52 +0800

    first

--- src/com/example/webviewdemo/MainActivity.java
+++ src/com/example/webviewdemo/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
 	private ProgressBar mProgressBar;
 	private TextView titleTextView;
 	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
